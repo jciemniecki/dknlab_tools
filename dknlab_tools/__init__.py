a875611f9bd7f2e0df52a9a02efb7c5b8c299883
John Ciemniecki <john.ciemniecki@Johns-Mac-mini.local>
2021-07-14 12:55:16 -0700

    fixed imaris module initialization bug

--- dknlab_tools/__init__.py
+++ dknlab_tools/__init__.py
@@ -2,8 +2,9 @@ from . import tecan
 from . import biotek
 from . import spectramax
 from . import viz
+from . import imaris
 
 
 __author__ = 'John Ciemniecki'
 __email__ = 'jciemnie@caltech.edu'
-__version__ = '1.0.1'
+__version__ = '1.0.3'
